0742fdf3591803614ae8c1deb18f8e2ae66fb90a
Tim Haak <tim@haak.co>
2020-05-20 20:26:03 +0200

    Updates to fix passport

--- src/app/constants/ConstantAuth.ts
+++ src/app/constants/ConstantAuth.ts
@@ -1,7 +1,7 @@
 export const ConstantAuth = {
   authUrl: '/api/v1/oauth/token',
-  client_id: '1',
-  client_secret: '3fj8GVtEohIX82OHk4DsrrdYbH10LYq3zd338bkU',
+  client_id: '2',
+  client_secret: 'fmEatZVcOEdpcRRkbjfNF7wCmOZlKUR89suhdFGx',
   redirect_uri: 'http://localhost',
   scope: '*',
 };
